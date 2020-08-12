b9bc5ffa3bc1b92789e920c516d29bfeea339c9e
CFCode-git <435184766@qq.com>
2020-08-12 17:36:25 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,87 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-height:100vh;
+  display: flex;
+  flex-direction:column;
+`;
+
+const Main = styled.div`
+  flex-grow:1;
+`;
+
+const Nav = styled.nav`
+  border:1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width:33.33333%;
+      text-align:center;
+      padding:16px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>hi</div>
+    <Router>
+        <Wrapper>
+          <Main>
+            <Switch>
+              <Route path="/tags">
+                <Tags />
+              </Route>
+              <Route path="/money">
+                <Money />
+              </Route>
+              <Route path="/statistics">
+                <Statistics />
+              </Route>
+              <Redirect exact from="/" to="/money"/>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </Main>
+          <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Users</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+function NoMatch(){
+ return <h2>找不到页面</h2>
 }
 
 export default App;
