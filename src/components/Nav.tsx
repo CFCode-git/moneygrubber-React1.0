c4c8086961d9c77db77b7b56c870a3b67914fa3c
CFCode-git <435184766@qq.com>
2020-08-13 21:53:40 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Icon from './icon';
 
 
-
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow:0 0 3px rgba(0,0,0,0.25);
@@ -13,14 +12,16 @@ const NavWrapper = styled.nav`
     > li {
       width:33.33333%;
       text-align:center;
-      display: flex;
-      flex-direction:column;
-      padding:4px 0;
-      justify-content: center;
-      align-items:center;
-      .icon{
-        width:24px;
-        height:24px;
+      > a {
+        display: flex;
+        flex-direction:column;
+        padding:4px 0;
+        justify-content: center;
+        align-items:center;
+        .icon{
+          width:24px;
+          height:24px;
+        }
       }
     }
   }
@@ -31,16 +32,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="label"/>
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="label"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="statistics"/>
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="statistics"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
