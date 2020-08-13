7f5738cd272e6eef95d5b59809f58882bca930e6
CFCode-git <435184766@qq.com>
2020-08-13 11:31:22 +0800

    添加 svg 文件，before eject

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Redirect
 } from "react-router-dom";
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   min-height:100vh;
@@ -18,17 +19,7 @@ const Main = styled.div`
   flex-grow:1;
 `;
 
-const Nav = styled.nav`
-  border:1px solid blue;
-  > ul {
-    display: flex;
-    > li {
-      width:33.33333%;
-      text-align:center;
-      padding:16px;
-    }
-  }
-`;
+
 
 function App() {
   return (
@@ -51,19 +42,7 @@ function App() {
               </Route>
             </Switch>
           </Main>
-          <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">Users</Link>
-            </li>
-          </ul>
-        </Nav>
+          <Nav/>
       </Wrapper>
     </Router>
   );
