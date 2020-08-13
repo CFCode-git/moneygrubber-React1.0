72facd5d2ea6ae4397230a0d8a4e65327810c139
CFCode-git <435184766@qq.com>
2020-08-13 22:16:54 +0800

    当前路由图标高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './icon';
 
@@ -22,6 +22,12 @@ const NavWrapper = styled.nav`
           width:24px;
           height:24px;
         }
+        &.selected{
+          color:#7ea465;
+          .icon{
+            fill:#7ea465;
+          }
+        }
       }
     }
   }
@@ -32,22 +38,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags" activeClassName="selected">
             <Icon name="label"/>
             标签页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name="money"/>
             记账页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="statistics"/>
             统计页
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
