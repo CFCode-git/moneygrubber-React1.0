79e182d0cc7e18c0e81642d6b873ea259e94a82c
CFCode-git <435184766@qq.com>
2020-08-13 16:43:49 +0800

    创建 Icon 组件，批量 require svg

---
+++ src/components/icon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
+
+/* require 一个文件夹 */
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+type Props = {
+  name: string
+}
+
+const Icon = (props: Props) => {
+  return (
+    <svg className="icon">
+      <use xlinkHref={'#' + props.name}/>
+    </svg>
+  );
+};
+
+export default Icon;
