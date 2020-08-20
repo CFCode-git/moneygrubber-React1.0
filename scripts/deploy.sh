b5bfcc66edfdacf635102ab558748859b01ee8ca
CFCode-git <435184766@qq.com>
2020-08-20 10:06:19 +0800

    部署到码云

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,12 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:CFCode-git/moneygrubber-React-website1.0.git &&
+
+# github
+#git remote add origin git@github.com:CFCode-git/moneygrubber-React-website1.0.git &&
+
+# gitee
+git remote add origin git@gitee.com:chowq/moneygrubber-react-website1.0.git &&
+
 git push -u origin master -f
 cd -
